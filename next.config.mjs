c8ce6784a8aac62d0b0ac6f57250752e17af18cc
sean <2579935313@qq.com>
2025-04-25 20:12:36 +0800

    fix path

--- next.config.mjs
+++ next.config.mjs
@@ -10,8 +10,16 @@ const nextConfig = {
     unoptimized: true,
   },
   output: 'export',
-  basePath: process.env.NEXT_PUBLIC_CUSTOM_DOMAIN ? '' : (process.env.NODE_ENV === 'production' ? '/feedme' : ''),
+  
+  // 只设置trailingSlash，确保所有路径都是一致的
+  trailingSlash: true,
+  
+  // 在生产环境下动态设置assetPrefix
+  assetPrefix: process.env.NODE_ENV === 'production' ? '.' : '',
+  
+  // 解决 fs 和 path 模块的问题
   webpack: (config, { isServer }) => {
+    // 为fs和path等Node.js模块提供空模拟
     if (!isServer) {
       config.resolve.fallback = {
         ...config.resolve.fallback,
